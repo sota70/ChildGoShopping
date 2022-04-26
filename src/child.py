e7dd7764dd5ca79ea1cc33492894a78c5b72ed2a
sota70 <sota52350@gmail.com>
2022-04-26 09:03:14 +0900

    change variable name shop to shopContent

--- src/child.py
+++ src/child.py
@@ -19,43 +19,43 @@ class Child:
     指定された商品を一つ買ったあと、もう一つの指定された商品を
     限度額になるまで買う
 
-    parameter shop: お店にある商品とその金額一覧
+    parameter shopContent: お店にある商品とその金額一覧
     parameter targetItem1: おつかいで買ってくる商品1つ目
     parameter targetItem2: おつかいで買ってくる商品2つ目
     """
-    def goShopping(
+    def goshopContentping(
         self, 
-        shop: dict[str, int],  
+        shopContent: dict[str, int],  
         targetItem1: str, 
         targetItem2: str
     ):
-        self.buyTargetItem(shop, targetItem1)
-        self.keepBuyingTargetItem(shop, targetItem2)
+        self.buyTargetItem(shopContent, targetItem1)
+        self.keepBuyingTargetItem(shopContent, targetItem2)
 
     """
     指定された商品があった場合それを１つ買うメソッド
 
-    parameter shop: お店にある商品とその金額一覧
+    parameter shopContent: お店にある商品とその金額一覧
     parameter targetItem: 買ってくる商品
     """
-    def buyTargetItem(self, shop: dict[str, int], targetItem: str):
-        shopItems = shop.keys()
-        if not targetItem in shopItems: return
+    def buyTargetItem(self, shopContent: dict[str, int], targetItem: str):
+        shopContentItems = shopContent.keys()
+        if not targetItem in shopContentItems: return
         print(targetItem + "を買った")
-        targetItemPrice = shop[targetItem]
+        targetItemPrice = shopContent[targetItem]
         self.money -= targetItemPrice
 
     """
     指定された商品があった場合それを複数個買うメソッド
     所持金が事前に定めた限度額になるまで商品を買い続ける
 
-    parameter shop: お店にある商品とその金額一覧
+    parameter shopContent: お店にある商品とその金額一覧
     parameter targetItem: 買ってくる商品
     """
-    def keepBuyingTargetItem(self, shop: dict[str, int], targetItem: str):
-        shopItems = shop.keys()
-        if not targetItem in shopItems: return
-        targetItemPrice = shop[targetItem]
+    def keepBuyingTargetItem(self, shopContent: dict[str, int], targetItem: str):
+        shopContentItems = shopContent.keys()
+        if not targetItem in shopContentItems: return
+        targetItemPrice = shopContent[targetItem]
         while self.money >= 120:
             print(targetItem + "を買った")
             self.money -= targetItemPrice
--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 from child import Child
 
 def main():
-    shop = { 
+    shopContent = { 
         "しらたき":120, 
         "こんにゃく":150, 
         "どらやき":160 
     }
     child = Child(1000)
-    child.goShopping(shop, "こんにゃく", "どらやき")
+    child.goShopping(shopContent, "こんにゃく", "どらやき")
 
 main()
